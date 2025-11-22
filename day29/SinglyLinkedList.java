fae5715c308d4643d19685a0fec4d55adadb8594
PrepInstaPrime <gaurav.pandey@addaeducation.com>
2025-11-22 15:06:55 +0530

    day29

--- day29/SinglyLinkedList.java
+++ day29/SinglyLinkedList.java
@@ -28,12 +28,37 @@ public class SinglyLinkedList{
         }
         System.out.print("null");
     }
+    static void insertAtPlace(int data, int place){
+        Node temp=head;
+        Node newNode= new Node(data);
+        int count=1;
+        if(place<=1){
+            newNode.next=head;
+            head=newNode;
+            return;
+        }
+        while(temp.next!=null&&count!=place-1){
+            temp=temp.next;
+            count++;
+        }
+        if(count==place-1){
+            newNode.next=temp.next;
+            temp.next=newNode;
+            return;
+        }else{
+            System.out.println("Can't insert becuase place not found");
+        }
+
+    }
     public static void main(String[] args) {
         SinglyLinkedList sll=new SinglyLinkedList();
         sll.insert(1);
         sll.insert(2);
-        sll.insert(3);
         sll.insert(4);
+        sll.insert(5);
+        sll.insertAtPlace(3,3);
+        sll.insertAtPlace(0,1);
+        sll.insertAtPlace(6,7);
         sll.display();
     }
 }
